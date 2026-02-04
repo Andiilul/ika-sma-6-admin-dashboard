78ff404b8e699d56a44e748d78833f0ea0aa10fe
Andiilul <hafidamfa21d@student.unhas.ac.id>
2026-02-05 03:24:41 +0800

    fx: auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,28 +2,21 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
-
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
+    protected $fillable = ['name', 'email', 'password'];
 
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected $hidden = ['password', 'remember_token'];
 
     protected function casts(): array
     {
@@ -38,5 +31,14 @@ class User extends Authenticatable
     public function getRoleAttribute(): ?string
     {
         return $this->getRoleNames()->first();
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        // TEMPORARY: allow all users (to confirm this is the blocker)
+        // return true;
+
+        // Recommended: restrict to admins
+        return $this->hasAnyRole(['super_admin', 'admin']);
     }
 }
